fe14f99679e694f329ea74a3732722cb38f3d871
01101101 <sylvere.armange@gmail.com>
2016-08-29 01:46:28 +0200

    blinking buttons

--- src/Button.hx
+++ src/Button.hx
@@ -8,18 +8,45 @@ class Button extends Entity
 {
 	
 	public var icon:Entity;
-
+	
+	public var isBlinking:Bool;
+	var blinkTick:Int;
+	
+	var blinkSpeed:Int = 10;
+	
 	public function new ()
 	{
 		super();
 		
 		setAnim(Sprites.BUTTON);
 		frame = 0;
+		isBlinking = false;
+		blinkTick = blinkSpeed;
+	}
+	
+	public function setBlinking (b:Bool = true)
+	{
+		isBlinking = b;
+		blinkTick = blinkSpeed;
 	}
 	
 	override public function postUpdate ()
 	{
-		
+		if (isBlinking)
+		{
+			if (blinkTick > 0)
+			{
+				blinkTick--;
+				if (blinkTick <= 0)
+				{
+					if (frame == 0)			frame = 2;
+					else if (frame == 1)	frame = 3;
+					else if (frame == 2)	frame = 0;
+					else if (frame == 3)	frame = 1;
+					blinkTick = blinkSpeed;
+				}
+			}
+		}
 	}
 	
 }
